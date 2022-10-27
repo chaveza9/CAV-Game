f972fdb035f075f557a5edaf9e31ffea2f03e80e
Andres Chavez <aschavez@bu.edu>
2022-10-27 19:00:07 -0400

    fixed bugs for cav c trajectory

--- cav_game/maneuvers/maneuver.py
+++ cav_game/maneuvers/maneuver.py
@@ -1,99 +1,102 @@
 import matplotlib.pyplot as plt
 import shutil
-import os 
+import os
 import abc
 import jax.numpy as jnp
 from typing import List, Tuple, Dict
 from cav_game.dynamics.dynamics import ControlAffineDynamics
 
 import pyomo as pyo
+from pyomo.environ import *
 from pyomo.opt import SolverStatus, TerminationCondition
 
 # Make sure that ipopt is installed
-assert(shutil.which("ipopt") or os.path.isfile("ipopt"))
+assert (shutil.which("ipopt") or os.path.isfile("ipopt"))
+
 
 # Definition of maneuver class
 class Maneuver(metaclass=abc.ABCMeta):
     """Class for defining a maneuver. This class is inherited by the other maneuvers and implements the 
     common functions with respect to the optimization problem using bicycle dynamics."""
-    def __init__(self,vehicle: ControlAffineDynamics, x0: jnp.ndarray, params: dict, **kwargs):
+
+    def __init__(self, vehicle: ControlAffineDynamics, x0: jnp.ndarray, params: dict, **kwargs):
         # Create vehicle object
         self.vehicle = vehicle
         # Define Mandatory Parameters
-        self.cav_type = params["cav_type"] # CAV type can be "CAV1", "CAV2", or "CAVC"
+        self.cav_type = params["cav_type"]  # CAV type can be "CAV1", "CAV2", or "CAVC"
         # Define initial state
-        self._initial_state = x0     
+        self._initial_state = x0
         ## Define Optional Parameters
         # Optimization Parameters
-        self.n = params.get("n", 250 ) # Number of time steps
-        
-        opt_optiopns = {"acceptable_tol": 1e-8, 
-                    "acceptable_obj_change_tol" : 1e-8,  
-                    "max_iter" : 10000, 
-                    "print_level" : 3}
-        self.opt_options = params.get("opt_options", opt_optiopns) # Optimization options 
-        
+        self.n = params.get("n", 250)  # Number of time steps
+
+        opt_optiopns = {"acceptable_tol": 1e-8,
+                        "acceptable_obj_change_tol": 1e-8,
+                        "max_iter": 10000,
+                        "print_level": 3}
+        self.opt_options = params.get("opt_options", opt_optiopns)  # Optimization options
+
         ## Extract vehicle parameters
-        self.v_bounds = self.vehicle.v_bounds # bounds on the velocity
-        self.u_bounds = self.vehicle.u_bounds # bounds on the acceleration
-        
+        self.v_bounds = self.vehicle.v_bounds  # bounds on the velocity
+        self.u_bounds = self.vehicle.u_bounds  # bounds on the acceleration
+
         ## Define private variables
         # Flags
-        self._is_feasible = False # Flag to indicate if the optimization problem is feasible
+        self._is_feasible = False  # Flag to indicate if the optimization problem is feasible
         # Terminal States
-        self._terminal_time = 0 # Terminal time
-        self._terminal_state = x0 # Terminal state
+        self._terminal_time = 0  # Terminal time
+        self._terminal_state = x0  # Terminal state
         # Model placeholder
-        self._model = None # Placeholder for the model
-        self._model_instance = None # Placeholder for the model instance
-        self._results = None # Placeholder for the optimization results
-        
+        self._model = None  # Placeholder for the model
+        self._model_instance = None  # Placeholder for the model instance
+        self._results = None  # Placeholder for the optimization results
+
         # Initialize solver
-        self._opt = pyo.solverFactory('ipopt')
-        self._discretizer = pyo.dae.TransformationFactory('dae.collocation')
-    
+        self._opt = SolverFactory('ipopt')
+        self._discretizer = TransformationFactory('dae.collocation')
+
     # Abstract methods
-    
+
     @abc.abstractmethod
     def _define_model(self) -> None:
         """Function to define the optimization model with its variables"""
         pass
-    
+
     @abc.abstractmethod
     def _define_dae_constraints(self) -> None:
         """Function to define the DAE constraints"""
         pass
-    
+
     @abc.abstractmethod
     def _define_constraints(self) -> None:
         """Function to define the constraints"""
         pass
-    
+
     @abc.abstractmethod
     def _define_objective(self) -> None:
         """Function to define the objective"""
         pass
-    
+
     @abc.abstractmethod
     def _define_initial_conditions(self) -> None:
         """Function to define the initial conditions"""
         pass
-    
+
     @abc.abstractmethod
     def _solve(self) -> None:
         """Function to solve the optimization problem"""
         pass
-    
+
     @abc.abstractmethod
     def _extract_results(self) -> Dict[str, jnp.ndarray]:
         """Function to extract the results of the optimization problem"""
         pass
-    
-    
+
 
 # Longitudinal Maneuver
 class LongitudinalManeuver(Maneuver):
-    def __init__(self,vehicle: ControlAffineDynamics, x0: jnp.ndarray, x0_obst: Dict[str, jnp.array], params: dict, **kwargs): #TODO: change control affine dynamics to just vehicle
+    def __init__(self, vehicle: ControlAffineDynamics, x0: jnp.ndarray, x0_obst: Dict[str, jnp.array], params: dict,
+                 **kwargs):  # TODO: change control affine dynamics to just vehicle
         """
         Class for defining a longitudinal maneuver. This class is inherited by the other maneuvers and 
         implements the common functions with respect to the optimization problem using bicycle dynamics.
@@ -108,68 +111,77 @@ class LongitudinalManeuver(Maneuver):
         # Desired Terminal State
         self.v_des = params.get("v_des", 30)  # Desired velocity [m/s]
         # Safety Parameters
-        self.reaction_time = params.get("reaction_time", 0.6) # Vehicle Reaction time [s]
-        self.min_safe_distance = params.get("min_safe_distance", 2) # Minimum inter-vehicle safety distance [m]
+        self.reaction_time = params.get("reaction_time", 0.6)  # Vehicle Reaction time [s]
+        self.min_safe_distance = params.get("min_safe_distance", 2)  # Minimum inter-vehicle safety distance [m]
         # Long Tunning Parameters
-        self.alpha_time = params.get("alpha_time", 0.1 ) # Time penalty weight
-        self.alpha_control = params.get("alpha_control", 0.3) # Control penalty weight
-        self.alpha_speed = params.get("alpha_speed",0.6) # Speed penalty weight
+        self.alpha_time = params.get("alpha_time", 0.1)  # Time penalty weight
+        self.alpha_control = params.get("alpha_control", 0.3)  # Control penalty weight
+        self.alpha_speed = params.get("alpha_speed", 0.6)  # Speed penalty weight
         # Check that weights add up to 1
-        assert(self.alpha_time + self.alpha_control + self.alpha_speed == 1, "Weights must add up to 1")
-        
-        self.t_max =params.get("t_max", 15)# Maximum time [s]
-        self._initial_state_obst = x0_obst # List of initial states of the obstacle vehicles
-        
+        # assert (self.alpha_time + self.alpha_control + self.alpha_speed == 1, "Weights must add up to 1")
+
+        self.t_max = params.get("t_max", 15)  # Maximum time [s]
+        self._initial_state_obst = x0_obst  # List of initial states of the obstacle vehicles
+
         # Normalize the weights
-        self._beta_u = self.alpha_control/(jnp.max(jnp.array(self.u_bounds)**2)) # Acceleration cost weight
-        self._beta_t = self.alpha_time      # Minimum Time weight
-        self._beta_v = self.alpha_speed     # Desired velocity weight
-        
+        self._beta_u = self.alpha_control / (jnp.max(jnp.array(self.u_bounds) ** 2))  # Acceleration cost weight
+        self._beta_t = self.alpha_time  # Minimum Time weight
+        self._beta_v = self.alpha_speed  # Desired velocity weight
+
         # Define the optimization model
         self._define_model()
         self._define_constraints()
         self._define_dae_constraints()
         self._define_objective()
         self._define_initial_conditions()
+        self._define_solver()
 
-    def compute_longitudinal_trajectory(self, plot:bool = True, save_path: str = "", obstacle: bool = False, show:bool = True) -> Dict[str, jnp.ndarray]:
+    def compute_longitudinal_trajectory(self, plot: bool = True, save_path: str = "", obstacle: bool = False,
+                                        show: bool = True) -> Dict[str, jnp.ndarray]:
+        """Function to compute the longitudinal trajectory of the CAV"""
+        # self._model_instance = self._model.create_instance()
+        instance = self._model_instance
+        results = self._opt.solve(instance, tee=True, options=self.opt_options)
         # Solve the optimization problem
-        self._solve()
+        # self._solve()
         # Extract the results
-        trajectory  = self._extract_results()
+        trajectory = self._extract_results()
         # Generate the plots
         if plot:
             self._generate_trajectory_plots(save_path, obstacle, show)
-        
+
         return trajectory
-    
+
     def _extract_results(self) -> Dict[str, jnp.ndarray]:
         raise NotImplementedError("Define _extract_results in subclass")
-    
-    def _generate_trajectory_plots(self, save_path: str = "", obstacle: bool = False, show: bool = False) -> Tuple[plt.figure, List[plt.axes]]:
+
+    def _generate_trajectory_plots(self, save_path: str = "", obstacle: bool = False, show: bool = False) -> Tuple[
+        plt.figure, List[plt.axes]]:
         """Function to generate the plots of the trajectory and the control inputs"""
         model = self._model_instance
+
         # Extract the results
-        tsim = [t*model.tf.value for t in range(model.n+1)]
+        tsim = [t * model.tf.value for t in model.t]
         xsim = [model.x[t]() for t in model.t]
         vsim = [model.v[t]() for t in model.t]
         usim = [model.u[t]() for t in model.t]
-        
+
         if obstacle:
             xsim_obst = [model.x_obst[t]() for t in model.t]
-            safety_distance = [model.xobst[t]() - self.min_safe_distance - self.reaction_time*model.v[t]()  for t in model.t]
-        
+            safety_distance = [model.x_obst[t]() - self.min_safe_distance - self.reaction_time * model.v[t]() for t in
+                               model.t]
+
         # Plot the trajectory
-        
-        fig = plt.figure(figsize=(10,5))
+
+        fig = plt.figure(figsize=(10, 5))
         (ax1, ax2, ax3) = fig.subplots(3, sharex=True)
-        fig.suptitle('Trajectory ' + self.cav_type)    
+        fig.suptitle('Trajectory ' + self.cav_type)
         # Position vs Time
         ax1.plot(tsim, xsim, label='Ego Vehicle')
         if obstacle:
             ax1.plot(tsim, xsim_obst, label='Obstacle Vehicle', color='red')
-            ax1.plot(tsim, safety_distance, label='Safety Distance', color='green', linestyle='dashdotted')
-            ax1.legend(location='best')
+            ax1.plot(tsim, safety_distance, label='Safety Distance', color='green', linestyle='-.')
+            ax1.legend()
         ax1.grid(True)
         ax1.set_ylabel('Position [m]')
         # Velocity vs Time
@@ -181,37 +193,25 @@ class LongitudinalManeuver(Maneuver):
         ax3.set_ylabel('Acceleration [m/s^2]')
         ax3.set_ylabel('Time [s]')
         ax3.grid(True)
-        
+
         if save_path:
             fig.savefig(save_path)
-            
+
         if show:
+            fig.tight_layout()
             fig.show()
-            
+
         return (fig, [ax1, ax2, ax3])
-    
+
     def _solve(self) -> None:
         """Function to solve the optimization problem"""
         # Solve the optimization problem
-        results = self._opt.solve(self._model_instance, tee=True)
+        results = self._opt.solve(self._model_instance, tee=True, options = self.opt_options)
         # Check the results status       
-        if not((results.solver.status == SolverStatus.ok) and (results.solver.termination_condition == TerminationCondition.optimal)):
+        if not ((results.solver.status == SolverStatus.ok) and (
+                results.solver.termination_condition == TerminationCondition.optimal)):
             raise ValueError("Optimal solution not found", results.solver.status)
         else:
             print("Optimal solution found")
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-              
-        
-        
-        
+
+        self._results = results
--- cav_game/maneuvers/selfish.py
+++ cav_game/maneuvers/selfish.py
@@ -7,30 +7,31 @@ from cav_game.dynamics.car import ControlAffineDynamics
 from typing import List, Tuple, Dict
 
 class SelfishManeuver(LongitudinalManeuver):  
-    def __init__(self,vehicle: ControlAffineDynamics, x0: jnp.ndarray, x0_obst: Dict[str, jnp.array], params: dict, **kwargs)
+    def __init__(self,vehicle: ControlAffineDynamics, x0: jnp.ndarray, x0_obst: Dict[str, jnp.array], params: dict, **kwargs):
         #  initialize the parent class
         super().__init__(vehicle, x0, x0_obst, params, **kwargs)   
     
     def _define_model(self) -> None:
         # Initialize a parametric model
-        model = pyo.AbstractModel()
+        model = pyo.environ.ConcreteModel()
         # Define model parameters
-        model.x0 = pyo.Param(initialize=self._initial_state[0], mutable = True) # Initial position
-        model.v0 = pyo.Param(initialize=self._initial_state[0], mutable = True) # Initial speed
+        model.t = pyo.dae.ContinuousSet(bounds=(0, 1))
+        model.x0 = pyo.environ.Param(initialize=self._initial_state[0], mutable = True) # Initial position
+        model.v0 = pyo.environ.Param(initialize=self._initial_state[1], mutable = True) # Initial speed
         # Front obstacle parameters
-        model.x0_obst = pyo.Param(initialize=self._initial_state_obst['front'][0], mutable = True) # Initial position
-        model.v0_obst = pyo.Param(initialize=self._initial_state_obst['front'][1], mutable = True) # Initial speed
+        model.x0_obst = pyo.environ.Param(initialize=self._initial_state_obst['front'][0], mutable = True) # Initial position
+        model.v0_obst = pyo.environ.Param(initialize=self._initial_state_obst['front'][1], mutable = True) # Initial speed
         # Define vehicle variables
-        model.tf = pyo.Var(bounds=(0, self.alpha_time), initialize=10) # Terminal time
-        model.x = pyo.Var(model.t) # Position
-        model.v = pyo.Var(model.t, bounds=(self.v_bounds[0],self.v_bounds[1])) # Velocity
-        model.u = pyo.Var(model.t, bounds=(self.u_bounds[0],self.u_bounds[1])) # Acceleration
+        model.tf = pyo.environ.Var(bounds=(0, self.t_max)) # Terminal time
+        model.x = pyo.environ.Var(model.t) # Position
+        model.v = pyo.environ.Var(model.t, bounds=(self.v_bounds[0],self.v_bounds[1])) # Velocity
+        model.u = pyo.environ.Var(model.t, bounds=(self.u_bounds[0],self.u_bounds[1])) # Acceleration
         # Define obstacle variables
-        model.x_obst = pyo.Var(model.t) # Position
+        model.x_obst = pyo.environ.Var(model.t) # Position
         
         # Define Derivatives
-        model.xdot = pyo.dae.DerivativeVar(model.x, wrt=model.t)
-        model.vdot = pyo.dae.DerivativeVar(model.v, wrt=model.t)
+        model.x_dot = pyo.dae.DerivativeVar(model.x, wrt=model.t)
+        model.v_dot = pyo.dae.DerivativeVar(model.v, wrt=model.t)
         
         self._model = model
         
@@ -41,22 +42,22 @@ class SelfishManeuver(LongitudinalManeuver):
         # Define differential algebraic equations
         def ode_x(m, t):
             return m.x_dot[t] == m.v[t]*m.tf
-        model.ode_x = pyo.Constraint(model.t, rule=ode_x)
+        model.ode_x = pyo.environ.Constraint(model.t, rule=ode_x)
         def ode_v(m, t):
-            return m.y_dot[t] == m.u[t]*m.tf
-        model.ode_v = pyo.Constraint(model.t, rule=ode_v)
+            return m.v_dot[t] == m.u[t]*m.tf
+        model.ode_v = pyo.environ.Constraint(model.t, rule=ode_v)
     
     def _define_constraints(self) -> None:
         """Create model constraints """
         model = self._model
         # Define Obstalce Model
         def obstacle_model(m, t):
-            return m.x_obst[t] == m.x0_obst + m.v0_obst*t
-        model.obs_ode = pyo.Constraint(model.t, rule = obstacle_model)
+            return m.x_obst[t] == (m.x0_obst + m.v0_obst*t)*m.tf
+        model.obs_ode = pyo.environ.Constraint(model.t, rule = obstacle_model)
         # Define safety constraints
         def safety_distance(m, t):
             return m.x_obst[t] - m.x[t] >= self.reaction_time*m.v[t] + self.min_safe_distance
-        model.safety = pyo.Constraint(model.t, rule = safety_distance)
+        model.safety = pyo.environ.Constraint(model.t, rule = safety_distance)
         
     def _define_initial_conditions(self) -> None:
         """Define the initial conditions."""
@@ -69,14 +70,13 @@ class SelfishManeuver(LongitudinalManeuver):
         """Define the objective function."""
         model = self._model
         # Define objective function
-        def time_objective(m):
-            return m.tf*self.alpha_time*self.n
-        def speed_objective(m):
-            return pyo.dae.Integral(model.t, wrt=model.t, rule=lambda m, t: 0.5*self._beta_v*m.tf*(m.v[t]-self.v_des)**2)
-        def acceleration_objective(m):
-            return pyo.dae.Integral(model.t, wrt=model.t, rule=lambda m, t: 0.5*self._beta_u*m.tf*(m.u[t])**2)
+        model.time_objective =  model.tf*self.alpha_time*self.n
+        model.speed_objective =  self._beta_v*model.tf*(model.v[1]-self.v_des)**2
+        model.acceleration_objective = pyo.dae.Integral(model.t, wrt=model.t,
+                                                        rule=lambda m, t: 0.5*float(self._beta_u)*m.tf*(m.u[t])**2)
         
-        model.obj = pyo.Objective(rule=time_objective+speed_objective+acceleration_objective, sense=pyo.minimize)
+        model.obj = pyo.environ.Objective(rule=model.time_objective+model.speed_objective+model.acceleration_objective,
+                                  sense=pyo.environ.minimize)
         
     def _define_solver(self) -> None:
         """Initialize the solver."""
@@ -90,10 +90,10 @@ class SelfishManeuver(LongitudinalManeuver):
         
         trajectory = dict()
         # Extract solution
-        trajectory['t'] = jnp.array([model.t[i]() for i in model.t])
-        trajectory['x'] = jnp.array([model.x[i]() for i in model.t])
-        trajectory['v'] = jnp.array([model.v[i]() for i in model.t])
-        trajectory['u'] = jnp.array([model.u[i]() for i in model.t])
+        trajectory['t'] = [t*model.tf for t in model.t]
+        trajectory['x'] = [model.x[i]() for i in model.t]
+        trajectory['v'] = [model.v[i]() for i in model.t]
+        trajectory['u'] = [model.u[i]() for i in model.t]
         
         return trajectory
         
